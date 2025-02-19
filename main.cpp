df21f4296a1055e68164a233c838f2ff396c0911
Lineyier <lenovorus22@gmail.com>
2025-02-19 10:33:12 +0700

    ne budi zvera

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(int, char**){
     setlocale(LC_ALL,"Russian");
-    std::cout << "Разбудите Романыча плиз\n";
+    std::cout << "Не будите Романыча плиз\n";
    int a = 121123;
     int b = 234234;
     int с = 234234;
