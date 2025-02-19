af202ace11a5f44ee5c9382c66c8567d09f707b3
Khaltura <khalturin-yaroslav@mail.ru>
2025-02-19 10:28:30 +0700

    raboty rabotai

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int, char**){
+    setlocale(LC_ALL,"Russian");
+    std::cout << "Разбудите Романыча плиз\n";
+   int a = 121123;
+    int b = 234234;
+    int с = 234234;
+    std::cout << "Ответ: "<< a + b + с;
+    
+}
